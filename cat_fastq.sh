5b1a0509a6e1cf1a887fc47e2b09349175d6560a
Michael Steinbaugh <mike@steinbaugh.com>
2018-05-11 10:12:47 -0400

    Remove old top level scripts

--- cat_fastq.sh
+++
@@ -1,7 +0,0 @@
-# Copy the demultiplexed FASTQ files generated from raw Illumina BCL
-# InDrop v3: each FASTQ should contain `_R1` through `_R4`
-cat *_R1_001.fastq.gz > combined_R1.fastq.gz
-cat *_R2_001.fastq.gz > combined_R2.fastq.gz
-cat *_R3_001.fastq.gz > combined_R3.fastq.gz
-cat *_R4_001.fastq.gz > combined_R4.fastq.gz
-rm *_R[1-4]_001.fastq.gz
