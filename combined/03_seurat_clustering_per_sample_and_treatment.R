91f7ad2cc3b19587ac8c4529b7d5e1412b4f6834
Michael Steinbaugh <mike@steinbaugh.com>
2018-06-21 13:06:44 -0400

    Reorganize clustering scripts

--- combined/03_seurat_clustering_per_sample_and_treatment.R
+++ combined/03_seurat_clustering_per_sample_and_treatment.R
@@ -1,12 +1,9 @@
-library(bcbioSingleCell)  # v0.1.15
+library(bcbioSingleCell)
 library(rmarkdown)
 
 loadDataAsName(bcb = bcb_filtered, dir = "data/2018-06-18")
 data_dir <- file.path("data", Sys.Date())
-
 sampleNames(bcb)
-# blood2_CTTAATAG blood2_TAAGGCTC blood3_ATAGCCTT blood3_CTTAATAG blood3_TAAGGCTC blood3_TCGCATAA
-# "injured 1"   "uninjured 3"   "uninjured 4"     "injured 3"   "uninjured 5"     "injured 4"
 
 # Per sample ===================================================================
 per_sample_subsets <- subsetPerSample(bcb, assignAndSave = TRUE, dir = data_dir)
@@ -19,9 +16,10 @@ saveData(injured, uninjured, dir = data_dir)
 
 # Render =======================================================================
 bcb_files <- c(
-    per_sample_subsets,
+    all_samples = "data/2018-06-18/bcb_filtered.rda",
     injured = "data/2018-06-18/injured.rda",
-    uninjured = "data/2018-06-18/uninjured.rda"
+    uninjured = "data/2018-06-18/uninjured.rda",
+    per_sample_subsets
 )
 stopifnot(all(file.exists(bcb_files)))
 mapply(
@@ -29,7 +27,7 @@ mapply(
     seurat_name = paste(names(bcb_files), "seurat", sep = "_"),
     FUN = function(bcb_file, seurat_name) {
         render(
-            input = "02_seurat_clustering.Rmd",
+            input = "seurat_clustering.Rmd",
             output_file = paste0(seurat_name, ".html"),
             output_format = "html_document",
             params = list(
