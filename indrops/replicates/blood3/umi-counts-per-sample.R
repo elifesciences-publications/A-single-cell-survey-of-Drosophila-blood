d435c75b9fad012699cc2686ea8b8d38f7b4d286
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-27 14:26:27 -0500

    Clean up scripts for blood replicates

--- indrops/replicates/blood3/umi-counts-per-sample.R
+++
@@ -1,9 +0,0 @@
-library(bcbioSingleCell)
-library(tidyverse)
-load("data/R/2018-01-10/run3_2018_01_10.rda")
-
-metrics <- metrics(run3_2018_01_10)
-sample_summary <- metrics %>%
-    select(sampleName, nUMI, nGene, nCoding) %>%
-    group_by(sampleName) %>%
-    summarize_all(sum)
